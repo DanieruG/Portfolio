a66a08e6ee99a272b39a4b61915c4031eecb7bd4
DanieruG <danielgyabaah952@outlook.com>
2025-12-22 20:59:58 +0000

    Made several useful changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,65 @@
+"use client";
 import Image from "next/image";
+import { motion } from "framer-motion";
+import React from "react";
+import { AuroraBackground } from "@/components/ui/aurora-background";
 
-export default function Home() {
+export default function Hero() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <AuroraBackground className="bg-black">
+      <motion.div
+        initial={{ opacity: 0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+        className="relative h-full flex flex-col justify-center items-center"
+      >
+        <header className="absolute top-0 left-0 right-0 flex justify-center p-8">
+          {" "}
+          {/* This absolute here fixed a massive issue. Please cover it more thoroughly soon, just like every other piece of AI in here. */}
+          <div className="space-x-30 p-4 text-white backdrop-filter rounded-3xl backdrop-blur-sm bg-white/15">
+            <a href="#" className="font-semibold">
+              Projects
+            </a>
+            <a href="#" className="font-semibold">
+              Experience
+            </a>
+            <a href="#" className="font-semibold">
+              About Me
+            </a>
+          </div>
+        </header>
+        <section>
+          <div className="flex flex-row items-center space-x-4">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+              alt="Profile Placeholder"
+              src="./profile-picture.svg"
+              width="200"
+              height="200"
+              className="rounded-full"
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+            <div className="flex flex-col items-center space-y-2">
+              <h2 className="text-white text-5xl font-bold">
+                Hey, my name's Daniel Gyabaah.
+              </h2>
+              <p className="text-muted text-center w-100">
+                I am currently a 1st year Computer Science student @ The
+                University of Manchester.
+              </p>
+              <Image
+                alt="GHub"
+                src="./github.svg"
+                width="50"
+                height="50"
+                className="dark:invert"
+              />
+            </div>
+          </div>
+        </section>
+      </motion.div>
+    </AuroraBackground>
   );
 }
