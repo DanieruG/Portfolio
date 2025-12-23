7a609e851f4f4c1ceb399245835bc7aa8f2d1a48
DanieruG <danielgyabaah952@outlook.com>
2025-12-23 21:30:35 +0000

    Flexible project card generation, and design polishing

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,36 @@ import Link from "next/link";
 import { motion } from "framer-motion";
 import React from "react";
 import { AuroraBackground } from "@/components/ui/aurora-background";
+import { cn } from "@/lib/utils";
 
 export default function Hero() {
+  const projects = [
+    {
+      title: "Conjugator",
+      description: "Something I built with Python, HTML, CSS and Javascript.",
+      image: "/conjugator.png",
+      technologies: [
+        "Python",
+        "HTML",
+        "CSS",
+        "Javascript",
+        "JQuery",
+        "Flask",
+        "SQL",
+      ],
+    },
+
+    {
+      title: "Pomodoro Timer",
+      description:
+        "A small timer project I did to improve my skills in Tkinter.",
+      image: "/placeholder.jpg",
+      technologies: ["Python", "Tkinter"],
+    },
+  ];
+
   return (
-    <div>
+    <div className="overflow-hidden w-full">
       <AuroraBackground className="bg-black">
         <motion.div
           initial={{ opacity: 0, y: 40 }}
@@ -22,7 +48,7 @@ export default function Hero() {
           <header className="absolute top-0 left-0 right-0 flex justify-center p-8">
             {" "}
             {/* This absolute here fixed a massive issue. Please cover it more thoroughly soon, just like every other piece of AI in here. */}
-            <div className="space-x-30 p-4 text-white backdrop-filter rounded-3xl backdrop-blur-sm bg-white/15">
+            <div className="space-x-2 p-4 text-white backdrop-filter rounded-3xl backdrop-blur-sm bg-white/15 md:space-x-30">
               <a href="#" className="font-semibold">
                 Projects
               </a>
@@ -38,7 +64,7 @@ export default function Hero() {
           {/* MAIN HEADER */}
 
           <section>
-            <div className="flex flex-row items-center space-x-4">
+            <div className="flex flex-col items-center space-x-4 md:flex-row">
               <Image
                 alt="Profile Placeholder"
                 src="/IMG_6980.jpg"
@@ -48,11 +74,11 @@ export default function Hero() {
               />
               {/* aspect-square = container remains 1:1 and object-cover here to fill the square and crop the edges... very interesting */}
               <div className="flex flex-col items-center space-y-3">
-                <h2 className="text-5xl font-bold bg-linear-to-r from-blue-500 to-blue-200 bg-clip-text text-transparent py-1">
+                <h2 className="text-xl font-bold bg-linear-to-r from-blue-500 to-blue-200 bg-clip-text text-transparent py-1 md:text-5xl sm:text-center">
                   Hey, my name's Daniel Gyabaah.
                 </h2>
 
-                <p className="text-transparent text-center w-100 bg-linear-to-r from-blue-200 to-blue-500 bg-clip-text">
+                <p className="text-transparent text-center w-50 bg-linear-to-r from-blue-200 to-blue-500 bg-clip-text">
                   I am currently a 1st year Computer Science student @ The
                   University of Manchester.
                 </p>
@@ -90,36 +116,50 @@ export default function Hero() {
               A collection of things I've built for school, or just for fun!
             </p>
           </div>
+
           {/* This is where card generation needs to happen! */}
-          <div className="flex flex-row p-8">
-            <div className="max-w-sm rounded overflow-hidden bg-zinc-900">
-              <Image
-                alt="placeholder"
-                src="/conjugator.png"
-                width={300}
-                height={100}
-                className="w-full aspect-video object-cover"
-              />
-              <div className="px-6 py-4 border border-zinc-700">
-                <div className="text-xl font-bold text-center py-3">
-                  Conjugator
-                </div>
-                <p className="text-sm text-zinc-400 ml-2">
-                  Built with Python, HTML, CSS and a little JavaScript.
-                </p>
-                <div className="m-2 space-x-4">
-                  <span className="inline-flex items-center transition-all hover:bg-white hover:text-black rounded-md bg-gray-400/10 px-2 py-1 text-xs font-medium text-gray-400 inset-ring inset-ring-gray-400/20">
-                    Python
-                  </span>
-                </div>
-                <div className="flex justify-end">
-                  {/* The button styling here was the work of AI */}
-                  <button className="px-3 py-2 text-sm font-semibold text-white border border-white/20 rounded-md hover:bg-white hover:text-black transition-all duration-300">
-                    View Demo
-                  </button>
+          <div className="flex flex-col gap-6 p-8 justify-center md:flex-row">
+            {/* Gap is great because it works both ways! */}
+            {projects.map((project) => (
+              <div className="max-w-sm flex flex-col flex-1 border border-zinc-700 rounded overflow-hidden bg-zinc-900">
+                <Image
+                  alt="placeholder"
+                  src={project.image}
+                  width={300}
+                  height={100}
+                  className="w-full aspect-video object-cover"
+                />
+                <div className="px-6 py-4 flex flex-col grow">
+                  <div className="text-xl font-bold text-center py-3">
+                    {project.title}
+                  </div>
+                  <p className="text-sm text-zinc-400 ml-2">
+                    {project.description}
+                  </p>
+
+                  <div className="m-2 space-x-3 space-y-2">
+                    <p className="text-zinc-500 font-semibold text-sm">
+                      Technologies used:
+                    </p>
+                    {project.technologies.map((tech) => (
+                      <span
+                        key={tech}
+                        className="inline-flex items-center transition-all hover:bg-white hover:text-black rounded-md bg-gray-400/10 px-2 py-1 text-xs font-medium text-gray-400 inset-ring inset-ring-gray-400/20"
+                      >
+                        {tech}
+                      </span>
+                    ))}
+                  </div>
+
+                  <div className="flex justify-end mt-auto">
+                    {/* The button styling here was the work of AI */}
+                    <button className="px-3 py-2 text-sm font-semibold text-white border border-white/20 rounded-md hover:bg-white hover:text-black transition-all duration-300">
+                      View Demo
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
+            ))}
           </div>
         </section>
       </div>
